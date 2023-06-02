1671a35428fbcb485c0b1a883f5aaaa5fff43292
Julien Bertram <bertramjulien@gmail.com>
2023-06-02 16:22:17 -0400

    i18next initialized + navbar switch button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,26 @@
 import {Link} from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+const lngs = {
+  en: { nativeName: "English" },
+  fr: { nativeName: 'Français'}
+};
 
 export default function Navbar() {
+  const { t, i18n } = useTranslation();
   return(
     <nav className='navbar'>
-      <h2 className='nav-title'>Julien's Blog</h2>
+      <h2 className='nav-title'>{t('navbar.title')}</h2>
       <div className='nav-link'>
-        <Link to='/'>Home</Link>
-        <Link to='/post'>Posts</Link>
+        <Link to='/'>{t('navbar.home')}</Link>
+        <Link to='/post'>{t('navbar.posts')}</Link>
+        <div>
+          {Object.keys(lngs).map((lng) => (
+              <button key={lng} style={{ display: i18n.resolvedLanguage === lng ? 'none' : '' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+                {lngs[lng].nativeName}
+              </button>
+            ))}
+        </div>
       </div>
     </nav>
   );
